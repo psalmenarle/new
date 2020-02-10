0ce0a5239937bd2e18d42115f41b0c28a666aed3
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-02-10 13:13:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,32 @@
-
+<?php
  $domOBJ = new DOMDocument();
  $domOBJ->load("https://www.nasa.gov/rss/dyn/breaking_news.rss");  
-
+ 
  $content = $domOBJ->getElementsByTagName("item");
-
+ 
  ?>
-  <h1>NASA Breaking News</h1>
+  <h1>BTS</h1>
+
     <?php
  foreach( $content as $data )
+ { ?>
  { 
+
+
+<?php
    $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
-  $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
    $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
-   $guid = $data->getElementsByTagName("guid")->item(0)->nodeValue;
-   $pubDate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
+   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
    echo "
    <ul>
-            <h2>$title</h2>
+            <h2><a href = '$link'$title </a></h2>
               <ul>
-                  <li> $description </li>
-                  <li> $link </li>
-                  <li> $guid </li>
-                  <li>$pubDate</li>
-              </ul>
+                  <li>$link </li>
+                  <li>$description</li>
+                  </ul>
     </ul>
             ";
+  ?>
+<?php
  }
 ?>
