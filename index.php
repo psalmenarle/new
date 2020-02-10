e2394e05a204981eda7fdb3860b6c527125b6134
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-02-10 12:03:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
  $domOBJ = new DOMDocument();
- $domOBJ->load("https://rarbg.to/rss.php");  
+ $domOBJ->load("https://showrss.info/other/shows.rss");  
  
  $content = $domOBJ->getElementsByTagName("item");
  
@@ -15,6 +15,7 @@
 
 
    $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+  $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
    $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
    $guid = $data->getElementsByTagName("guid")->item(0)->nodeValue;
    $pubDate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
@@ -23,6 +24,7 @@
             <h2>BTS members</h2>
               <ul>
                   <li> $title </li>
+                  <li> $description </li>
                   <li> $link </li>
                   <li> $guid </li>
                   <li>$pubDate</li>
