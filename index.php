d3b07d9caa747d0be37631dffb2259e1d69fa39b
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-02-10 12:47:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
-$rss = new DOMDocument();
-$rss->load("https://www.nasa.gov/rss/dyn/breaking_news.rss");
 
-$feed = array();
-foreach ($rss->getElementsByTagName('item') as $node) {
-    $item = array ( 
-        'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
-        'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
-        'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
-        'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue,
-        );
-    array_push($feed, $item);
-}
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://www.nasa.gov/rss/dyn/breaking_news.rss");  
+
+ $content = $domOBJ->getElementsByTagName("item");
+
+ ?>
+  <h1>NASA Breaking News</h1>
+    <?php
+ foreach( $content as $data )
+ { 
+   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+  $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+   $guid = $data->getElementsByTagName("guid")->item(0)->nodeValue;
+   $pubDate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
+   echo "
+   <ul>
+            <h2>$title</h2>
+              <ul>
+                  <li> $description </li>
+                  <li> $link </li>
+                  <li> $guid </li>
+                  <li>$pubDate</li>
+              </ul>
+    </ul>
+            ";
+ }
+?>
