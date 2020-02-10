beedf7bad1e14b1990340db0ea2dc16841cecf9e
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-02-10 11:51:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,33 @@
-<!DOCTYPE html>
-<html>
-<body onload="loadXMLDoc()">
+<?php
 
-<h2>Using the XMLHttpRequest Object</h2>
 
-<div id="demo">
-</div>
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://rarbg.to/rss.php");  
+ 
+ $content = $domOBJ->getElementsByTagName("item");
+ 
+ ?>
+  <h1>MOVIES & TV SHOWS</h1>
 
-<script>
-function loadXMLDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "emails.xml", true);
-  xhttp.send();
-}
-</script>
-</body>
-</html>
+    <?php
+ foreach( $content as $data )
+ { 
+
+
+   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+   $guid = $data->getElementsByTagName("guid")->item(0)->nodeValue;
+   $pubDate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
+   echo "
+   <ul>
+            <h2>BTS members</h2>
+              <ul>
+                  <li> $title </li>
+                  <li> $link </li>
+                  <li> $guid </li>
+                  <li>$pubDate</li>
+              </ul>
+    </ul>
+            ";
+ }
+?>
